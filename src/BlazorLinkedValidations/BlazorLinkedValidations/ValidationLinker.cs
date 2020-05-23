f5593d9a9d9f916dd23900cd2943e5046095d69b
Plevi1337 <plevimaster@gmail.com>
2020-05-23 18:41:38 +0200

    build and comments added

--- src/BlazorLinkedValidations/BlazorLinkedValidations/ValidationLinker.cs
+++ src/BlazorLinkedValidations/BlazorLinkedValidations/ValidationLinker.cs
@@ -4,16 +4,33 @@ using System;
 
 namespace BlazorLinkedValidations
 {
+  /// <summary>
+  /// Validation linker component, fires a changed event for all the linked fields.
+  /// The events fired by this component won't fire another round of events. Only the direct links work.
+  /// </summary>
   public class ValidationLinker : ComponentBase, IDisposable
   {
+
+    /// <summary>
+    /// Cascading Edit Context parameter, if null the OnInitialized method throws an ArgumentNullException.
+    /// </summary>
     [CascadingParameter]
     EditContext EditContext { get; set; }
 
+    /// <summary>
+    /// Linker parameter, if null the OnInitialized method throws an ArgumentNullException
+    /// </summary>
     [Parameter]
     public ValidationLinkerBase Linker { get; set; }
 
+    /// <summary>
+    /// Indicates whether the this component is firing the changed events or another source.
+    /// </summary>
     private bool _isFiring = false;
 
+    /// <summary>
+    /// Checks the component parameters for nullability and subscribes to the FieldChanged event of the EditContext.
+    /// </summary>
     protected override void OnInitialized()
     {
       _ = EditContext ?? throw new ArgumentNullException(nameof(EditContext), "ValidationLinker component needs a cascading EditContext parameter.");
@@ -21,6 +38,11 @@ namespace BlazorLinkedValidations
       EditContext.OnFieldChanged += OnFieldChanged;
     }
 
+    /// <summary>
+    /// Field Change event handler. Fires a changed event for every linked field.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void OnFieldChanged(object sender, FieldChangedEventArgs e)
     {
       if (!_isFiring)
@@ -34,6 +56,9 @@ namespace BlazorLinkedValidations
       }
     }
 
+    /// <summary>
+    /// Unsubscribes from the FieldChanged event of the EditContext.
+    /// </summary>
     public void Dispose()
     {
       EditContext.OnFieldChanged -= OnFieldChanged;
--- src/BlazorLinkedValidations/BlazorLinkedValidations/ValidationLinkerBase.cs
+++ src/BlazorLinkedValidations/BlazorLinkedValidations/ValidationLinkerBase.cs
@@ -6,10 +6,18 @@ using System.Linq;
 
 namespace BlazorLinkedValidations
 {
+  /// <summary>
+  /// This is the abstract base class of the linkers.
+  /// </summary>
   public abstract class ValidationLinkerBase
   {
     private Dictionary<FieldIdentifier, IEnumerable<FieldIdentifier>> _links = new Dictionary<FieldIdentifier, IEnumerable<FieldIdentifier>>();
 
+    /// <summary>
+    /// Adds the targets to the list of fields to validate when the origin field gets edited.
+    /// </summary>
+    /// <param name="origin">The origin of the links.</param>
+    /// <param name="targets">The targets of the links.</param>
     protected void Link(Expression<Func<object>> origin, params Expression<Func<object>>[] targets)
     {
       FieldIdentifier originField = FieldIdentifier.Create(origin);
@@ -24,6 +32,11 @@ namespace BlazorLinkedValidations
       }
     }
 
+    /// <summary>
+    /// Returns the fields linked to the parameter field.
+    /// </summary>
+    /// <param name="origin">The origin field.</param>
+    /// <returns>The list of the linked fields, empty if there are none.</returns>
     public IEnumerable<FieldIdentifier> GetLinkedFields(FieldIdentifier origin) =>
       _links.ContainsKey(origin) ? _links[origin] : Enumerable.Empty<FieldIdentifier>();
   }
